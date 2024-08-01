cabe798b25f796c4e3c4c844d4bca0a9176344d1
leedoyeon849 <do83430208@gmail.com>
2024-08-02 04:27:12 +0900

    feat(Router): Add safari button to web view controller

--- Projects/DI/RouterDIModule/Router/Sources/WebViewController.swift
+++ Projects/DI/RouterDIModule/Router/Sources/WebViewController.swift
@@ -46,21 +46,32 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         webView.load(request)
         
         // 사파리로 연결될 버튼 추가
-//        let safariButton = UIButton(type: .system)
-//        safariButton.setTitle("Open in Safari", for: .normal)
-//        safariButton.addTarget(self, action: #selector(openInSafari), for: .touchUpInside)
-//        safariButton.backgroundColor = .fullWhite
-//        view.addSubview(safariButton)
-//        
-//        // 버튼 제약 조건 설정
-//        safariButton.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            safariButton.bottomAnchor.constraint(
-//                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
-//                constant: -20
-//            ),
-//            safariButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-//        ])
+        let safariButton = UIButton(type: .system)
+        safariButton.setTitle("Safari로 연결", for: .normal)
+        safariButton.setTitleColor(UIColor.white, for: .normal)
+        safariButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        safariButton.backgroundColor = UIColor(red: 0/255, green: 160/255, blue: 235/255, alpha: 1)
+        safariButton.layer.cornerRadius = 8
+        safariButton.addTarget(self, action: #selector(openInSafari), for: .touchUpInside)
+        view.addSubview(safariButton)
+        
+        // 버튼 제약 조건 설정
+        safariButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            safariButton.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                constant: 0
+            ),
+            safariButton.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: 16
+            ),
+            safariButton.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -16
+            ),
+            safariButton.heightAnchor.constraint(equalToConstant: 54)
+        ])
     }
     
     @objc func openInSafari() {
